596ca75dff19cc562d2eeeeea94f0c991ef67940
Dylan Moreland <79415431+elffjs@users.noreply.github.com>
2023-08-24 04:25:38 -0400

    Update drivly_valuation_service.go

--- internal/core/services/drivly_valuation_service.go
+++ internal/core/services/drivly_valuation_service.go
@@ -37,7 +37,7 @@ type drivlyValuationService struct {
 	log       *zerolog.Logger
 }
 
-func NewDrivlyValuationService(DBS func() *db.ReaderWriter, log *zerolog.Logger, settings *config.Settings, ddSvc DeviceDefinitionsAPIService, uddSvc UserDeviceDataAPIService, udSvc UserDeviceService) DrivlyValuationService {
+func NewDrivlyValuationService(DBS func() *db.ReaderWriter, log *zerolog.Logger, settings *config.Settings, ddSvc DeviceDefinitionsAPIService, uddSvc UserDeviceDataAPIService, udSvc UserDeviceAPIService) DrivlyValuationService {
 	return &drivlyValuationService{
 		dbs:       DBS,
 		log:       log,
