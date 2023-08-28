a8b9b9012cdaca33152f4db3fb7f8d1510562392
Eduardo Rodriguez <eduardorodriguez@Eduardos-MacBook-Pro.local>
2023-08-28 15:50:39 -0500

    [PRO-1892] change unit tests

--- internal/controllers/valuations_controller_test.go
+++ internal/controllers/valuations_controller_test.go
@@ -4,14 +4,11 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
-	"io"
 	"testing"
 
 	"github.com/DIMO-Network/devices-api/pkg/grpc"
 	"github.com/DIMO-Network/shared/db"
-	core "github.com/DIMO-Network/valuations-api/internal/core/models"
 	mock_services "github.com/DIMO-Network/valuations-api/internal/core/services/mocks"
-	"github.com/DIMO-Network/valuations-api/internal/infrastructure/db/models"
 	"github.com/DIMO-Network/valuations-api/internal/infrastructure/dbtest"
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang/mock/gomock"
@@ -20,9 +17,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go"
-	"github.com/tidwall/gjson"
-	"github.com/volatiletech/null/v8"
-	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 const migrationsDirRelPath = "../infrastructure/db/migrations"
@@ -85,24 +79,10 @@ func TestValuationsControllerTestSuite(t *testing.T) {
 	suite.Run(t, new(ValuationsControllerTestSuite))
 }
 
-//go:embed test_drivly_pricing_by_vin.json
-var testDrivlyPricingJSON string
-
-//go:embed test_drivly_pricing2.json
-var testDrivlyPricing2JSON string
-
-//go:embed test_vincario_valuation.json
-var testVincarioValuationJSON string
-
 func (s *ValuationsControllerTestSuite) TestGetDeviceValuations_Format1() {
 	// arrange db, insert some user_devices
-	ddID := ksuid.New().String()
 	udID := ksuid.New().String()
 	vin := "vinny"
-
-	_ = SetupCreateValuationsData(s.T(), ddID, udID, vin, map[string][]byte{
-		"DrivlyPricingMetadata": []byte(testDrivlyPricingJSON),
-	}, s.pdb)
 
 	s.userDeviceSvc.EXPECT().GetUserDevice(gomock.Any(), udID).Return(&grpc.UserDevice{
 		Id:           udID,
@@ -114,32 +94,16 @@ func (s *ValuationsControllerTestSuite) TestGetDeviceValuations_Format1() {
 
 	request := dbtest.BuildRequest("GET", fmt.Sprintf("/user/devices/%s/valuations", udID), "")
 	response, _ := s.app.Test(request)
-	body, _ := io.ReadAll(response.Body)
 
 	assert.Equal(s.T(), fiber.StatusOK, response.StatusCode)
-
-	assert.Equal(s.T(), 1, int(gjson.GetBytes(body, "valuationSets.#").Int()))
-	assert.Equal(s.T(), 49957, int(gjson.GetBytes(body, "valuationSets.#(vendor=drivly).mileage").Int()))
-	assert.Equal(s.T(), 49957, int(gjson.GetBytes(body, "valuationSets.#(vendor=drivly).odometer").Int()))
-	assert.Equal(s.T(), "miles", gjson.GetBytes(body, "valuationSets.#(vendor=drivly).odometerUnit").String())
-	assert.Equal(s.T(), 54123, int(gjson.GetBytes(body, "valuationSets.#(vendor=drivly).retail").Int()))
-	//54123 + 50151 / 2
-	assert.Equal(s.T(), 52137, int(gjson.GetBytes(body, "valuationSets.#(vendor=drivly).userDisplayPrice").Int()))
-	assert.Equal(s.T(), "USD", gjson.GetBytes(body, "valuationSets.#(vendor=drivly).currency").String())
-	// 49040 + 52173 + 49241 / 3 = 50151
-	assert.Equal(s.T(), 50151, int(gjson.GetBytes(body, "valuationSets.#(vendor=drivly).tradeIn").Int()))
-	assert.Equal(s.T(), 50151, int(gjson.GetBytes(body, "valuationSets.#(vendor=drivly).tradeInAverage").Int()))
 }
 
 func (s *ValuationsControllerTestSuite) TestGetDeviceValuations_Format2() {
 	// this is the other format we're seeing coming from drivly for pricing
 	// arrange db, insert some user_devices
-	ddID := ksuid.New().String()
 	udID := ksuid.New().String()
 	vin := "vinny"
-	_ = SetupCreateValuationsData(s.T(), ddID, udID, vin, map[string][]byte{
-		"DrivlyPricingMetadata": []byte(testDrivlyPricing2JSON),
-	}, s.pdb)
+
 	s.userDeviceSvc.EXPECT().GetUserDevice(gomock.Any(), udID).Return(&grpc.UserDevice{
 		Id:           udID,
 		UserId:       userID,
@@ -150,27 +114,15 @@ func (s *ValuationsControllerTestSuite) TestGetDeviceValuations_Format2() {
 
 	request := dbtest.BuildRequest("GET", fmt.Sprintf("/user/devices/%s/valuations", udID), "")
 	response, _ := s.app.Test(request)
-	body, _ := io.ReadAll(response.Body)
 
 	assert.Equal(s.T(), fiber.StatusOK, response.StatusCode)
-
-	assert.Equal(s.T(), 1, int(gjson.GetBytes(body, "valuationSets.#").Int()))
-	// mileage comes from request metadata, but it is also sometimes returned by payload
-	assert.Equal(s.T(), 50702, int(gjson.GetBytes(body, "valuationSets.#(vendor=drivly).mileage").Int()))
-	assert.Equal(s.T(), 40611, int(gjson.GetBytes(body, "valuationSets.#(vendor=drivly).tradeIn").Int()))
-	assert.Equal(s.T(), 50803, int(gjson.GetBytes(body, "valuationSets.#(vendor=drivly).retail").Int()))
 }
 
 func (s *ValuationsControllerTestSuite) TestGetDeviceValuations_Vincario() {
 	// this is the other format we're seeing coming from drivly for pricing
 	// arrange db, insert some user_devices
-	ddID := ksuid.New().String()
 	udID := ksuid.New().String()
 	vin := "vinny"
-
-	_ = SetupCreateValuationsData(s.T(), ddID, udID, vin, map[string][]byte{
-		"VincarioMetadata": []byte(testVincarioValuationJSON),
-	}, s.pdb)
 
 	s.userDeviceSvc.EXPECT().GetUserDevice(gomock.Any(), udID).Return(&grpc.UserDevice{
 		Id:           udID,
@@ -179,63 +131,17 @@ func (s *ValuationsControllerTestSuite) TestGetDeviceValuations_Vincario() {
 		Vin:          &vin,
 		CountryCode:  "USA",
 	}, nil)
-
-	// ? TODO: check how to retrieve this from db in configuration
-	s.userDeviceSvc.EXPECT().GetUserDeviceValuations(gomock.Any(), udID, "USA").Return(&core.DeviceValuation{
-		ValuationSets: []core.ValuationSet{
-			{
-				Vendor:           "vincario",
-				Updated:          "",
-				Mileage:          30137,
-				ZipCode:          "",
-				TradeInSource:    "",
-				TradeIn:          44800,
-				TradeInClean:     0,
-				TradeInAverage:   0,
-				TradeInRough:     0,
-				RetailSource:     "",
-				Retail:           55200,
-				RetailClean:      0,
-				RetailAverage:    0,
-				RetailRough:      0,
-				OdometerUnit:     "km",
-				Odometer:         30137,
-				UserDisplayPrice: 51440,
-				Currency:         "EUR",
-			},
-		},
-	})
 
 	request := dbtest.BuildRequest("GET", fmt.Sprintf("/user/devices/%s/valuations", udID), "")
 	response, _ := s.app.Test(request, 2000)
-	body, _ := io.ReadAll(response.Body)
 
 	assert.Equal(s.T(), fiber.StatusOK, response.StatusCode)
-
-	assert.Equal(s.T(), 1, int(gjson.GetBytes(body, "valuationSets.#").Int()))
-	// mileage comes from request metadata, but it is also sometimes returned by payload
-	assert.Equal(s.T(), 30137, int(gjson.GetBytes(body, "valuationSets.#(vendor=vincario).mileage").Int()))
-	assert.Equal(s.T(), 30137, int(gjson.GetBytes(body, "valuationSets.#(vendor=vincario).odometer").Int()))
-	assert.Equal(s.T(), "km", gjson.GetBytes(body, "valuationSets.#(vendor=vincario).odometerUnit").String())
-	assert.Equal(s.T(), "EUR", gjson.GetBytes(body, "valuationSets.#(vendor=vincario).currency").String())
-
-	assert.Equal(s.T(), 44800, int(gjson.GetBytes(body, "valuationSets.#(vendor=vincario).tradeIn").Int()))
-	assert.Equal(s.T(), 55200, int(gjson.GetBytes(body, "valuationSets.#(vendor=vincario).retail").Int()))
-	assert.Equal(s.T(), 51440, int(gjson.GetBytes(body, "valuationSets.#(vendor=vincario).userDisplayPrice").Int()))
 }
-
-//go:embed test_drivly_offers_by_vin.json
-var testDrivlyOffersJSON string
 
 func (s *ValuationsControllerTestSuite) TestGetDeviceOffers() {
 	// arrange db, insert some user_devices
-	ddID := ksuid.New().String()
 	udID := ksuid.New().String()
 	vin := "vinny"
-
-	_ = SetupCreateValuationsData(s.T(), ddID, udID, vin, map[string][]byte{
-		"OfferMetadata": []byte(testDrivlyOffersJSON),
-	}, s.pdb)
 
 	s.userDeviceSvc.EXPECT().GetUserDevice(gomock.Any(), udID).Return(&grpc.UserDevice{
 		Id:           udID,
@@ -244,60 +150,9 @@ func (s *ValuationsControllerTestSuite) TestGetDeviceOffers() {
 		Vin:          &vin,
 		CountryCode:  "USA",
 	}, nil)
-
-	// ? TODO: check how to retrieve this from db in configuration
-	s.userDeviceSvc.EXPECT().GetUserDeviceOffers(gomock.Any(), udID).Return(&core.DeviceOffer{
-		OfferSets: []core.OfferSet{
-			{
-				Source:  "drivly",
-				Updated: "",
-				Mileage: 0,
-				ZipCode: "",
-				Offers:  []core.Offer{{Vendor: "vroom", Price: 10123, Error: "Error in v1/acquisition/appraisal POST", DeclineReason: ""}, {Vendor: "carvana", Price: 10123, URL: "", Error: "", Grade: "", DeclineReason: "Make[Ford],Model[Mustang Mach-E],Year[2022] is not eligible for offer."}, {Vendor: "carmax", Price: 10123, DeclineReason: "", Error: "Error in v1/acquisition/appraisal POST"}},
-			},
-		},
-	})
 
 	request := dbtest.BuildRequest("GET", fmt.Sprintf("/user/devices/%s/offers", udID), "")
 	response, err := s.app.Test(request)
 	require.NoError(s.T(), err)
 	require.Equal(s.T(), fiber.StatusOK, response.StatusCode)
-
-	body, _ := io.ReadAll(response.Body)
-
-	assert.Equal(s.T(), 1, int(gjson.GetBytes(body, "offerSets.#").Int()))
-	assert.Equal(s.T(), "drivly", gjson.GetBytes(body, "offerSets.0.source").String())
-	assert.Equal(s.T(), 3, int(gjson.GetBytes(body, "offerSets.0.offers.#").Int()))
-	assert.Equal(s.T(), "Error in v1/acquisition/appraisal POST",
-		gjson.GetBytes(body, "offerSets.0.offers.#(vendor=vroom).error").String())
-	assert.Equal(s.T(), 10123, int(gjson.GetBytes(body, "offerSets.0.offers.#(vendor=carvana).price").Int()))
-	assert.Equal(s.T(), "Make[Ford],Model[Mustang Mach-E],Year[2022] is not eligible for offer.",
-		gjson.GetBytes(body, "offerSets.0.offers.#(vendor=carmax).declineReason").String())
-}
-
-func SetupCreateValuationsData(t *testing.T, ddID, userDeviceID, vin string, md map[string][]byte, pdb db.Store) *models.Valuation {
-	val := models.Valuation{
-		ID:                 ksuid.New().String(),
-		DeviceDefinitionID: null.StringFrom(ddID),
-		Vin:                vin,
-		UserDeviceID:       null.StringFrom(userDeviceID),
-		RequestMetadata:    null.JSONFrom([]byte(`{"mileage":49957,"zipCode":"48216"}`)), // default request metadata
-	}
-	if rmd, ok := md["RequestMetadata"]; ok {
-		val.RequestMetadata = null.JSONFrom(rmd)
-	}
-	if omd, ok := md["OfferMetadata"]; ok {
-		val.OfferMetadata = null.JSONFrom(omd)
-	}
-	if vmd, ok := md["VincarioMetadata"]; ok {
-		val.VincarioMetadata = null.JSONFrom(vmd)
-	}
-	if vmd, ok := md["DrivlyPricingMetadata"]; ok {
-		val.DrivlyPricingMetadata = null.JSONFrom(vmd)
-	}
-
-	err := val.Insert(context.Background(), pdb.DBS().Writer, boil.Infer())
-	assert.NoError(t, err)
-
-	return &val
 }
---
+++ internal/core/services/user_device_service_test.go
@@ -0,0 +1,198 @@
+package services
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"testing"
+
+	"github.com/DIMO-Network/shared/db"
+	"github.com/DIMO-Network/valuations-api/internal/infrastructure/db/models"
+	"github.com/DIMO-Network/valuations-api/internal/infrastructure/dbtest"
+	"github.com/segmentio/ksuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+const migrationsDirRelPath = "../infrastructure/db/migrations"
+const userID = "2TqxFTIQPZ3gnUPi3Pdb3eEZDx4"
+
+type UserDeviceServiceTestSuite struct {
+	suite.Suite
+	pdb       db.Store
+	container testcontainers.Container
+	ctx       context.Context
+	svc       UserDeviceAPIService
+}
+
+//go:embed test_drivly_offers_by_vin.json
+var testDrivlyOffersJSON string
+
+//go:embed test_drivly_pricing_by_vin.json
+var testDrivlyPricingJSON string
+
+//go:embed test_drivly_pricing2.json
+var testDrivlyPricing2JSON string
+
+//go:embed test_vincario_valuation.json
+var testVincarioValuationJSON string
+
+// SetupSuite starts container db
+func (s *UserDeviceServiceTestSuite) SetupSuite() {
+	s.ctx = context.Background()
+	s.pdb, s.container = dbtest.StartContainerDatabase(s.ctx, "valuations_api", s.T(), migrationsDirRelPath)
+	logger := dbtest.Logger()
+
+	s.svc = NewUserDeviceService(nil, s.pdb.DBS, logger)
+
+	var err error
+
+	if err != nil {
+		s.T().Fatal(err)
+	}
+}
+
+func (s *UserDeviceServiceTestSuite) SetupTest() {
+
+}
+
+// TearDownTest after each test truncate tables
+func (s *UserDeviceServiceTestSuite) TearDownTest() {
+	dbtest.TruncateTables(s.pdb.DBS().Writer.DB, s.T())
+}
+
+// TearDownSuite cleanup at end by terminating container
+func (s *UserDeviceServiceTestSuite) TearDownSuite() {
+	fmt.Printf("shutting down postgres at with session: %s \n", s.container.SessionID())
+	if err := s.container.Terminate(s.ctx); err != nil {
+		s.T().Fatal(err)
+	}
+}
+
+// Test Runner
+func TestUserDeviceServiceTestSuite(t *testing.T) {
+	suite.Run(t, new(UserDeviceServiceTestSuite))
+}
+
+func (s *UserDeviceServiceTestSuite) TestGetUserDeviceValuations_Format1() {
+	// setup
+	ddID := ksuid.New().String()
+	vin := "VINDIESEL12312322"
+	userDeviceID := ksuid.New().String()
+	_ = SetupCreateValuationsData(s.T(), ddID, userDeviceID, vin, map[string][]byte{
+		"DrivlyPricingMetadata": []byte(testDrivlyPricingJSON),
+	}, s.pdb)
+
+	// test
+	valuations, err := s.svc.GetUserDeviceValuations(s.ctx, userDeviceID, vin)
+	assert.NoError(s.T(), err)
+	assert.Len(s.T(), valuations, 1)
+
+	assert.Equal(s.T(), 1, len(valuations.ValuationSets))
+	assert.Equal(s.T(), 49957, valuations.ValuationSets[0].Mileage)
+	assert.Equal(s.T(), 49957, valuations.ValuationSets[0].Mileage)
+	assert.Equal(s.T(), "miles", valuations.ValuationSets[0].OdometerUnit)
+	assert.Equal(s.T(), 54123, valuations.ValuationSets[0].Retail)
+	//54123 + 50151 / 2
+	assert.Equal(s.T(), 52137, valuations.ValuationSets[0].RetailAverage)
+	assert.Equal(s.T(), "USD", valuations.ValuationSets[0].Currency)
+	// 49040 + 52173 + 49241 / 3 = 50151
+	assert.Equal(s.T(), 50151, valuations.ValuationSets[0].TradeIn)
+	assert.Equal(s.T(), 50151, valuations.ValuationSets[0].TradeInAverage)
+}
+
+func (s *UserDeviceServiceTestSuite) TestGetUserDeviceValuations_Format2() {
+	ddID := ksuid.New().String()
+	udID := ksuid.New().String()
+	vin := "vinny"
+	_ = SetupCreateValuationsData(s.T(), ddID, udID, vin, map[string][]byte{
+		"DrivlyPricingMetadata": []byte(testDrivlyPricing2JSON),
+	}, s.pdb)
+
+	valuations, err := s.svc.GetUserDeviceValuations(s.ctx, udID, vin)
+
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), 1, len(valuations.ValuationSets))
+	// mileage comes from request metadata, but it is also sometimes returned by payload
+	assert.Equal(s.T(), 50702, valuations.ValuationSets[0].Mileage)
+	assert.Equal(s.T(), 40611, valuations.ValuationSets[0].TradeIn)
+	assert.Equal(s.T(), 50803, valuations.ValuationSets[0].Retail)
+}
+
+func (s *UserDeviceServiceTestSuite) TestGetUserDeviceValuations_Vincario() {
+	ddID := ksuid.New().String()
+	udID := ksuid.New().String()
+	vin := "vinny"
+
+	_ = SetupCreateValuationsData(s.T(), ddID, udID, vin, map[string][]byte{
+		"VincarioMetadata": []byte(testVincarioValuationJSON),
+	}, s.pdb)
+
+	valuations, err := s.svc.GetUserDeviceValuations(s.ctx, udID, vin)
+
+	assert.NoError(s.T(), err)
+	assert.Equal(s.T(), 1, len(valuations.ValuationSets))
+	// mileage comes from request metadata, but it is also sometimes returned by payload
+	assert.Equal(s.T(), 30137, valuations.ValuationSets[0].Mileage)
+	assert.Equal(s.T(), 30137, valuations.ValuationSets[0].Odometer)
+	assert.Equal(s.T(), "km", valuations.ValuationSets[0].OdometerUnit)
+	assert.Equal(s.T(), "EUR", valuations.ValuationSets[0].Currency)
+
+	assert.Equal(s.T(), 44800, valuations.ValuationSets[0].TradeIn)
+	assert.Equal(s.T(), 55200, valuations.ValuationSets[0].Retail)
+	assert.Equal(s.T(), 51440, valuations.ValuationSets[0].UserDisplayPrice)
+}
+
+func (s *UserDeviceServiceTestSuite) TestGetUserDeviceOffers() {
+	// arrange db, insert some user_devices
+	ddID := ksuid.New().String()
+	udID := ksuid.New().String()
+	vin := "vinny"
+
+	_ = SetupCreateValuationsData(s.T(), ddID, udID, vin, map[string][]byte{
+		"OfferMetadata": []byte(testDrivlyOffersJSON),
+	}, s.pdb)
+
+	deviceOffers, err := s.svc.GetUserDeviceOffers(s.ctx, udID)
+
+	assert.NoError(s.T(), err)
+
+	assert.Equal(s.T(), 1, len(deviceOffers.OfferSets))
+	assert.Equal(s.T(), "drivly", deviceOffers.OfferSets[0].Source)
+	assert.Equal(s.T(), 3, len(deviceOffers.OfferSets[0].Offers))
+	assert.Equal(s.T(), "Error in v1/acquisition/appraisal POST",
+		deviceOffers.OfferSets[0].Offers[0].DeclineReason)
+	assert.Equal(s.T(), 10123, deviceOffers.OfferSets[0].Offers[1].Price)
+	assert.Equal(s.T(), "Make[Ford],Model[Mustang Mach-E],Year[2022] is not eligible for offer.",
+		deviceOffers.OfferSets[0].Offers[2].DeclineReason)
+}
+
+func SetupCreateValuationsData(t *testing.T, ddID, userDeviceID, vin string, md map[string][]byte, pdb db.Store) *models.Valuation {
+	val := models.Valuation{
+		ID:                 ksuid.New().String(),
+		DeviceDefinitionID: null.StringFrom(ddID),
+		Vin:                vin,
+		UserDeviceID:       null.StringFrom(userDeviceID),
+		RequestMetadata:    null.JSONFrom([]byte(`{"mileage":49957,"zipCode":"48216"}`)), // default request metadata
+	}
+	if rmd, ok := md["RequestMetadata"]; ok {
+		val.RequestMetadata = null.JSONFrom(rmd)
+	}
+	if omd, ok := md["OfferMetadata"]; ok {
+		val.OfferMetadata = null.JSONFrom(omd)
+	}
+	if vmd, ok := md["VincarioMetadata"]; ok {
+		val.VincarioMetadata = null.JSONFrom(vmd)
+	}
+	if vmd, ok := md["DrivlyPricingMetadata"]; ok {
+		val.DrivlyPricingMetadata = null.JSONFrom(vmd)
+	}
+
+	err := val.Insert(context.Background(), pdb.DBS().Writer, boil.Infer())
+	assert.NoError(t, err)
+
+	return &val
+}
