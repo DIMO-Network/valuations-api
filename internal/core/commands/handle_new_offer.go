4cac96842340908227d4e8ac0d57fb846cee18a1
Eduardo Rodriguez <eduardorodriguez@Eduardos-MacBook-Pro.local>
2023-09-12 11:20:25 -0500

    [SM-11] add more logs

--- internal/core/commands/handle_new_offer.go
+++ internal/core/commands/handle_new_offer.go
@@ -9,21 +9,29 @@ import (
 )
 
 func (h *runValuationCommandHandler) ExecuteOfferSync(ctx context.Context) error {
+
 	sub, err := h.NATSSvc.JetStream.PullSubscribe(h.NATSSvc.OfferSubject, h.NATSSvc.OfferDurableConsumer, nats.AckWait(h.NATSSvc.AckTimeout))
+
 	if err != nil {
+		h.logger.Err(err).Msg("failed to subscribe to nats at offer sync")
 		return err
 	}
 
 	for {
 		msgs, err := sub.Fetch(10, nats.MaxWait(h.NATSSvc.AckTimeout))
+
 		if err != nil {
+			h.logger.Err(err).Msg("failed to fetch messages from nats at offer sync")
 			return err
 		}
 
 		for _, msg := range msgs {
 			var payload core.OfferRequest
 			err := json.Unmarshal(msg.Data, &payload)
+
 			if err != nil {
+				h.nak(msg)
+				h.logger.Err(err).Str("payload", string(msg.Data)).Msg("failed to process offer request due to invalid payload")
 				return err
 			}
 
@@ -31,14 +39,14 @@ func (h *runValuationCommandHandler) ExecuteOfferSync(ctx context.Context) error
 
 			if err != nil {
 				h.nak(msg)
-				h.logger.Err(err).Str("payload", string(msg.Data)).Msg("failed to process offer request")
+				h.logger.Err(err).Str("payload", string(msg.Data)).Msg("failed to process offer request due to internal error")
 				return err
 			}
 
 			h.logger.Info().Str("payload", string(msg.Data)).Msgf("processing offer request with status %s", status)
 
 			if err := msg.Ack(); err != nil {
-				h.logger.Err(err).Msg("message ack failed")
+				h.logger.Err(err).Msg("message ack failed for offer sync")
 			}
 		}
 	}
