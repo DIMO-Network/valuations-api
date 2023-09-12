868d9ee534c7d25581179099ce02dec8c3b04d58
Eduardo Rodriguez <eduardorodriguez@Eduardos-MacBook-Pro.local>
2023-09-12 13:33:15 -0500

    [SM-11] change queue processing

--- internal/core/commands/handle_new_offer.go
+++ internal/core/commands/handle_new_offer.go
@@ -2,9 +2,11 @@ package commands
 
 import (
 	"context"
+
 	"encoding/json"
 
 	core "github.com/DIMO-Network/valuations-api/internal/core/models"
+
 	"github.com/nats-io/nats.go"
 )
 
@@ -17,38 +19,58 @@ func (h *runValuationCommandHandler) ExecuteOfferSync(ctx context.Context) error
 		return err
 	}
 
+	h.logger.Info().Msg("subscribed to nats at offer sync")
+
 	for {
 		msgs, err := sub.Fetch(1, nats.MaxWait(h.NATSSvc.AckTimeout))
 
 		if err != nil {
+			if err == nats.ErrTimeout {
+				h.logger.Info().Msg("no messages found at offer sync")
+				continue
+			}
+
 			h.logger.Err(err).Msg("failed to fetch messages from nats at offer sync")
 			return err
 		}
 
 		for _, msg := range msgs {
-			var payload core.OfferRequest
-			err := json.Unmarshal(msg.Data, &payload)
+			mtd, err := msg.Metadata()
 
 			if err != nil {
-				//h.nak(msg)
-				msg.Ack()
-				h.logger.Err(err).Str("payload", string(msg.Data)).Msg("failed to process offer request due to invalid payload")
-				//return err
+				h.nak(msg)
+				h.logger.Err(err).Msg("failed to process offer request due to invalid payload")
+				continue
 			}
 
-			status, err := h.drivlyValuationService.PullOffer(ctx, payload.UserDeviceID)
+			select {
+			case <-ctx.Done():
+				h.logger.Info().Msg("context cancelled at offer sync")
+				return nil
+			default:
+				var payload core.OfferRequest
 
-			if err != nil {
-				//h.nak(msg)
-				msg.Ack()
-				h.logger.Err(err).Str("payload", string(msg.Data)).Msg("failed to process offer request due to internal error")
-				//return err
-			}
+				err := json.Unmarshal(msg.Data, &payload)
 
-			h.logger.Info().Str("payload", string(msg.Data)).Msgf("processing offer request with status %s", status)
+				if err != nil {
+					h.nak(msg)
+					h.logger.Err(err).Str("payload", string(msg.Data)).Msg("failed to process offer request due to invalid payload")
+					continue
+				}
 
-			if err := msg.Ack(); err != nil {
-				h.logger.Err(err).Msg("message ack failed for offer sync")
+				status, err := h.drivlyValuationService.PullOffer(ctx, payload.UserDeviceID)
+
+				if err != nil {
+					h.nak(msg)
+					h.logger.Err(err).Str("payload", string(msg.Data)).Msg("failed to process offer request due to internal error")
+					continue
+				}
+
+				h.logger.Info().Str("payload", string(msg.Data)).Msgf("processing offer request %v with status %s", mtd.NumDelivered, status)
+
+				if err := msg.Ack(); err != nil {
+					h.logger.Err(err).Msg("message ack failed for offer sync")
+				}
 			}
 		}
 	}
