df02e8f452fae0f342f477a033c54ba59a343faa
Eduardo Rodriguez <eduardorodriguez@Eduardos-MacBook-Pro.local>
2023-09-12 11:55:23 -0500

    [SM-11] return to normal status

--- internal/core/commands/handle_new_offer.go
+++ internal/core/commands/handle_new_offer.go
@@ -18,7 +18,7 @@ func (h *runValuationCommandHandler) ExecuteOfferSync(ctx context.Context) error
 	}
 
 	for {
-		msgs, err := sub.Fetch(10, nats.MaxWait(h.NATSSvc.AckTimeout))
+		msgs, err := sub.Fetch(1, nats.MaxWait(h.NATSSvc.AckTimeout))
 
 		if err != nil {
 			h.logger.Err(err).Msg("failed to fetch messages from nats at offer sync")
@@ -30,17 +30,17 @@ func (h *runValuationCommandHandler) ExecuteOfferSync(ctx context.Context) error
 			err := json.Unmarshal(msg.Data, &payload)
 
 			if err != nil {
-				msg.Ack()
+				h.nak(msg)
 				h.logger.Err(err).Str("payload", string(msg.Data)).Msg("failed to process offer request due to invalid payload")
-				//return err
+				return err
 			}
 
 			status, err := h.drivlyValuationService.PullOffer(ctx, payload.UserDeviceID)
 
 			if err != nil {
-				msg.Ack()
+				h.nak(msg)
 				h.logger.Err(err).Str("payload", string(msg.Data)).Msg("failed to process offer request due to internal error")
-				//return err
+				return err
 			}
 
 			h.logger.Info().Str("payload", string(msg.Data)).Msgf("processing offer request with status %s", status)
