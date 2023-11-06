fa2a62786d59d720a41c33bfe23f4777a4ee3f29
James Reategui <JamesReate@users.noreply.github.com>
2023-11-06 12:20:57 -0500

    tweaks to logging with valuation runner

--- cmd/valuations-api/pull_valuations.go
+++ cmd/valuations-api/pull_valuations.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"flag"
 
-	"os"
-
 	"github.com/DIMO-Network/shared/db"
 	"github.com/DIMO-Network/valuations-api/internal/config"
 	"github.com/DIMO-Network/valuations-api/internal/core/commands"
@@ -21,30 +19,27 @@ type loadValuationsCmd struct {
 	ddSvc         services.DeviceDefinitionsAPIService
 	userDeviceSvc services.UserDeviceAPIService
 	deviceDataSvc services.UserDeviceDataAPIService
+	wmi           *string
 }
 
-func (*loadValuationsCmd) Name() string     { return "valuations-pull" }
-func (*loadValuationsCmd) Synopsis() string { return "valuations-pull args to stdout." }
+func (*loadValuationsCmd) Name() string { return "pull-valuations" }
+func (*loadValuationsCmd) Synopsis() string {
+	return "pull-valuations runs through all connected cars and gets their valuation"
+}
 func (*loadValuationsCmd) Usage() string {
-	return `valuations-pull`
+	return `pull-valuations -wmi <WMI 3 char>`
 }
 
 // nolint
 func (p *loadValuationsCmd) SetFlags(f *flag.FlagSet) {
-
+	f.StringVar(p.wmi, "wmi", "", "WMI filter option to only get valuations for these")
 }
 
 func (p *loadValuationsCmd) Execute(ctx context.Context, _ *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
 	p.logger.Info().Msgf("Pull VIN info, valuations and pricing from driv.ly for USA and valuations from Vincario for EUR")
 	wmi := ""
-	if len(os.Args) > 2 {
-		// parse out vin WMI code to filter on
-		for i, a := range os.Args {
-			if a == "--wmi" {
-				wmi = os.Args[i+1]
-				break
-			}
-		}
+	if p.wmi != nil {
+		wmi = *p.wmi
 	}
 
 	handler := commands.NewLoadVinVerifiedValuationCommandHandler(p.pdb.DBS, p.logger, &p.settings, p.userDeviceSvc, p.ddSvc, p.deviceDataSvc)
