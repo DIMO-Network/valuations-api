e3064c49ad00eeb81347fecee6389b3a795d2f49
James Reategui <JamesReate@users.noreply.github.com>
2024-04-20 08:41:36 -0400

    fallbacks for token and vin, less logging if event type mismatch

--- internal/core/services/vehicle_mint_consumer.go
+++ internal/core/services/vehicle_mint_consumer.go
@@ -47,11 +47,17 @@ func (i *vehicleMintValuationIngest) ProcessVehicleMintMsg(ctx goka.Context, msg
 	event := msg.(*VehicleMintEvent)
 	// event.ID is the userDeviceId, set from devices-api
 	if event.Type != "com.dimo.zone.device.mint" {
-		i.logger.Info().Msgf("not processing event since of type: %s", event.Type) // change this to debug level after testing
+		i.logger.Debug().Msgf("not processing event since of type: %s", event.Type) // change this to debug level after testing
 	}
 	localLog := i.logger.With().Str("userDeviceId", event.ID).Logger()
 	// change below to debug once validate
 	localLog.Info().Str("payload", string(event.Data)).Msg("processing vehicle mint event for valuation/offer trigger")
+
+	userDevice, err := i.userDeviceService.GetUserDevice(ctx.Context(), event.ID)
+	if err != nil {
+		localLog.Error().Msg("unable to find user device")
+		return
+	}
 
 	jsonBytes, err := event.Data.MarshalJSON()
 	if err != nil {
@@ -62,15 +68,24 @@ func (i *vehicleMintValuationIngest) ProcessVehicleMintMsg(ctx goka.Context, msg
 	tokenID := gjson.GetBytes(jsonBytes, "nft.tokenId").Uint()
 	localLog = localLog.With().Str("vin", vin).Uint64("tokenId", tokenID).Logger()
 
+	// ideally the event has vin and tokenId, but have legacy fallbacks in case
 	if len(vin) == 0 {
-		localLog.Error().Msg("vin was empty")
-		return
+		if userDevice.Vin != nil {
+			vin = *userDevice.Vin
+		} else {
+			localLog.Error().Msg("skipping - no vin found in payload or userDevice")
+			return
+		}
 	}
-	userDevice, err := i.userDeviceService.GetUserDevice(ctx.Context(), event.ID)
-	if err != nil {
-		localLog.Error().Msg("unable to find user device")
-		return
+	if tokenID == 0 {
+		if userDevice.TokenId != nil {
+			tokenID = *userDevice.TokenId
+		} else {
+			localLog.Error().Msg("skipping - no tokenId found in payload or userDevice")
+			return
+		}
 	}
+
 	localLog = localLog.With().Str("country", userDevice.CountryCode).Str("deviceDefinitionId", userDevice.DeviceDefinitionId).Logger()
 	// todo: move tests over
 	// we currently have two vendors for valuations
